e4ca82adf261f2eb5aabf95d0e6820eb58d31643
Thomas <contact.fuzeblocks@gmail.com>
2025-01-03 19:01:15 +0100

    Update ListHomeCommand with new message,style,clickable message.

--- src/main/java/fr/fuzeblocks/homeplugin/commands/ListHomeCommand.java
+++ src/main/java/fr/fuzeblocks/homeplugin/commands/ListHomeCommand.java
@@ -2,6 +2,10 @@ package fr.fuzeblocks.homeplugin.commands;
 
 import fr.fuzeblocks.homeplugin.HomePlugin;
 import fr.fuzeblocks.homeplugin.home.HomeManager;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.*;
+import net.md_5.bungee.api.chat.hover.content.Content;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -14,11 +18,46 @@ public class ListHomeCommand implements CommandExecutor {
         if (sender instanceof Player) {
             Player player = ((Player) sender).getPlayer();
             HomeManager homeManager = HomePlugin.getHomeManager();
-            player.sendMessage("Home number : " + homeManager.getHomeNumber(player));
+            player.sendMessage("§bHome number : " + homeManager.getHomeNumber(player));
             for (String name : homeManager.getHomesName(player)) {
-                player.sendMessage("Home : " + name);
+                Location homeLocation = HomePlugin.getHomeManager().getHomeLocation(player,name);
+               sendHomeMessage(player,name,homeLocation);
             }
         }
         return false;
     }
+    private void sendHomeMessage(Player player, String homeName, Location homeLocation) {
+        player.sendMessage(ChatColor.GREEN + "View details for your home, " + ChatColor.BOLD + homeName);
+
+        BaseComponent[] locationComponent = new ComponentBuilder("Location: ")
+                .color(ChatColor.BLUE)
+                .append("x: " + homeLocation.getBlockX() + " y: " + homeLocation.getBlockY() + " z: " + homeLocation.getBlockZ())
+                .color(ChatColor.GOLD)
+                .create();
+        player.spigot().sendMessage(locationComponent);
+        TextComponent teleportTextComponent = new TextComponent("Teleport:");
+        teleportTextComponent.setColor(ChatColor.GRAY);
+        TextComponent teleportationTextComponent = new TextComponent(" [Click]");
+        teleportationTextComponent.setColor(ChatColor.YELLOW);
+        teleportationTextComponent.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Click to teleport at your home").create()));
+        teleportationTextComponent.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/home " + homeName));
+        player.spigot().sendMessage(teleportTextComponent,teleportationTextComponent);
+
+        TextComponent manageTextComponent = new TextComponent("Manage home:");
+        manageTextComponent.setColor(ChatColor.GRAY);
+
+        TextComponent relocateTextComponent = new TextComponent(" [Relocate home]");
+        relocateTextComponent.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Click to relocate your home").create()));
+        relocateTextComponent.setColor(ChatColor.GOLD);
+        relocateTextComponent.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/homerelocate"));
+
+        TextComponent deleteTextComponent = new TextComponent(" [Delete home]");
+        deleteTextComponent.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Click to delete your home").create()));
+        deleteTextComponent.setColor(ChatColor.YELLOW);
+        deleteTextComponent.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/delhome " + homeName));
+
+        player.spigot().sendMessage(manageTextComponent, relocateTextComponent, deleteTextComponent);
+
+    }
+
 }
