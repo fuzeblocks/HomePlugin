455ca41d1d3076a8faf7394ada295a0da1ac832b
fuzeblocks <villiotthomas18@gmail.com>
2025-10-21 18:32:16 +0200

    Remove debug logging from EconomyManager setup method

--- src/main/java/fr/fuzeblocks/homeplugin/economy/EconomyManager.java
+++ src/main/java/fr/fuzeblocks/homeplugin/economy/EconomyManager.java
@@ -15,32 +15,6 @@ public class EconomyManager {
     public static void setup(HomePlugin plugin) {
         economy = plugin.getEconomy();
         config = plugin.getConfig();
-
-        // --- DEBUG ---
-        if (config == null) {
-            plugin.getLogger().warning("§c[EconomyManager] config == null !");
-            return;
-        }
-
-        String path = "Config.Economy";
-        if (config.isConfigurationSection(path)) {
-            plugin.getLogger().info("[EconomyManager] Section '" + path + "' trouvée !");
-            double creation = config.getDouble(path + ".HomeCreationCost", -1);
-            double teleport = config.getDouble(path + ".Home-Teleport-Price", -1);
-            double tpa = config.getDouble(path + ".Tpa-Request-Price", -1);
-            double rtp = config.getDouble(path + ".Rtp-Price", -1);
-            boolean useEco = config.getBoolean(path + ".UseEconomy", false);
-
-            plugin.getLogger().info("  UseEconomy: " + useEco);
-            plugin.getLogger().info("  HomeCreationCost: " + creation);
-            plugin.getLogger().info("  Home-Teleport-Price: " + teleport);
-            plugin.getLogger().info("  Tpa-Request-Price: " + tpa);
-            plugin.getLogger().info("  Rtp-Price: " + rtp);
-        } else {
-            plugin.getLogger().warning("[EconomyManager] Section '" + path + "' introuvable dans config.yml !");
-            plugin.getLogger().info("Clés présentes à la racine : " + config.getKeys(false));
-        }
-        // --- FIN DEBUG ---
     }
 
 
