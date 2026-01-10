bc5623d2328f9b01f49a6516bdb59a549c351c19
VILLIOT-CORNILLON Thomas <villiotthomas18@gmail.coml>
2026-01-10 14:46:58 +0100

    Add GUI not supported message to multiple language files, and add prices on PlaceHolder Expansion

--- src/main/java/fr/fuzeblocks/homeplugin/placeholder/HomePluginExpansion.java
+++ src/main/java/fr/fuzeblocks/homeplugin/placeholder/HomePluginExpansion.java
@@ -1,6 +1,7 @@
 package fr.fuzeblocks.homeplugin.placeholder;
 
 import fr.fuzeblocks.homeplugin.HomePlugin;
+import fr.fuzeblocks.homeplugin.economy.EconomyManager;
 import fr.fuzeblocks.homeplugin.language.LanguageManager;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.Location;
@@ -17,11 +18,6 @@ public class HomePluginExpansion extends PlaceholderExpansion {
 
     private final HomePlugin homePlugin;
 
-    /**
-     * Instantiates a new Home plugin expansion.
-     *
-     * @param homePlugin the home plugin
-     */
     public HomePluginExpansion(HomePlugin homePlugin) {
         this.homePlugin = homePlugin;
     }
@@ -51,61 +47,74 @@ public class HomePluginExpansion extends PlaceholderExpansion {
 
         String lowerParams = params.toLowerCase();
 
-        switch (lowerParams) {
-            case "homes":
-                List<String> homeNames = HomePlugin.getHomeManager().getHomesName(player);
-                if (homeNames != null && !homeNames.isEmpty()) {
-                    return String.join(", ", homeNames);
-                } else {
-                    return translate("Messages.no-homes", "Aucun home");
-                }
-
-            case "homes_numbers":
-                return String.valueOf(HomePlugin.getHomeManager().getHomeNumber(player));
-
-            case "has_homes":
-                return HomePlugin.getHomeManager().getHomeNumber(player) > 0
-                        ? "true" : "false";
-        }
-
-        if (lowerParams.startsWith("home_location_")) {
+        if ("homes".equals(lowerParams)) {
+            return getHomeNames(player);
+        } else if ("homes_numbers".equals(lowerParams)) {
+            return getHomeNumber(player);
+        } else if ("has_homes".equals(lowerParams)) {
+            return hasHomes(player);
+        } else if ("home_teleport_price".equals(lowerParams)) {
+            return String.valueOf(EconomyManager.getHomeTeleportPrice());
+        } else if ("home_creation_price".equals(lowerParams)) {
+            return String.valueOf(EconomyManager.getHomeCreationCost());
+        } else if ("tpa_request_price".equals(lowerParams)) {
+            return String.valueOf(EconomyManager.getTpaRequestPrice());
+        } else if ("rtp_price".equals(lowerParams)) {
+            return String.valueOf(EconomyManager.getRtpPrice());
+        } else if (lowerParams.startsWith("home_location_")) {
             String name = params.substring("home_location_".length());
-            Location loc = HomePlugin.getHomeManager().getHomeLocation(player, name);
-            if (loc != null) {
-                String format = translate("Messages.home-location-format", "X: %.1f, Y: %.1f, Z: %.1f, Monde: %s");
-                return String.format(format, loc.getX(), loc.getY(), loc.getZ(), loc.getWorld().getName());
-            } else {
-                return translate("Messages.Unknown", "Inconnu");
-            }
-        }
-
-        if (lowerParams.startsWith("home_exists_")) {
+            return getHomeLocation(player, name);
+        } else if (lowerParams.startsWith("home_exists_")) {
             String name = params.substring("home_exists_".length());
-            boolean exists = HomePlugin.getHomeManager().exist(player, name);
-            return exists ? "true" : "false";
-        }
-
-        if (lowerParams.startsWith("home_world_")) {
+            return homeExists(player, name);
+        } else if (lowerParams.startsWith("home_world_")) {
             String name = params.substring("home_world_".length());
-            Location loc = HomePlugin.getHomeManager().getHomeLocation(player, name);
-            if (loc != null) {
-                return loc.getWorld().getName();
-            } else {
-                return translate("Messages.Unknown", "Inconnu");
-            }
-        }
-
-        if (lowerParams.startsWith("home_coordinates_")) {
+            return getHomeWorld(player, name);
+        } else if (lowerParams.startsWith("home_coordinates_")) {
             String name = params.substring("home_coordinates_".length());
-            Location loc = HomePlugin.getHomeManager().getHomeLocation(player, name);
-            if (loc != null) {
-                return String.format("%.1f %.1f %.1f", loc.getX(), loc.getY(), loc.getZ());
-            } else {
-                return translate("Messages.Unknown", "Inconnu");
-            }
+            return getHomeCoordinates(player, name);
         }
 
         return null;
+    }
+
+
+    private String getHomeNames(Player player) {
+        List<String> homeNames = HomePlugin.getHomeManager().getHomesName(player);
+        if (homeNames != null && !homeNames.isEmpty()) {
+            return String.join(", ", homeNames);
+        }
+        return translate("Messages.no-homes", "Aucun home");
+    }
+
+    private String getHomeNumber(Player player) {
+        return String.valueOf(HomePlugin.getHomeManager().getHomeNumber(player));
+    }
+
+    private String hasHomes(Player player) {
+        return HomePlugin.getHomeManager().getHomeNumber(player) > 0 ? "true" : "false";
+    }
+
+    private String getHomeLocation(Player player, String name) {
+        Location loc = HomePlugin.getHomeManager().getHomeLocation(player, name);
+        if (loc == null) return translate("Messages.Unknown", "Inconnu");
+        String format = translate("Messages.home-location-format", "X: %.1f, Y: %.1f, Z: %.1f, Monde: %s");
+        return String.format(format, loc.getX(), loc.getY(), loc.getZ(), loc.getWorld().getName());
+    }
+
+    private String homeExists(Player player, String name) {
+        return HomePlugin.getHomeManager().exist(player, name) ? "true" : "false";
+    }
+
+    private String getHomeWorld(Player player, String name) {
+        Location loc = HomePlugin.getHomeManager().getHomeLocation(player, name);
+        return (loc != null) ? loc.getWorld().getName() : translate("Messages.Unknown", "Inconnu");
+    }
+
+    private String getHomeCoordinates(Player player, String name) {
+        Location loc = HomePlugin.getHomeManager().getHomeLocation(player, name);
+        return (loc != null) ? String.format("%.1f %.1f %.1f", loc.getX(), loc.getY(), loc.getZ())
+                             : translate("Messages.Unknown", "Inconnu");
     }
 
     private String translate(String path, String defaultValue) {
